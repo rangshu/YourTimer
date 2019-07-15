08aa69600f4e2b0017fe90453927fdf20edaaef3
Onsemiro <soorink@gmail.com>
2019-07-16 00:06:25 +0900

    Initial Commit

---
+++ YourTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourTimer
+//
+//  Created by SooRin Kim on 16/07/2019.
+//  Copyright © 2019 SooRin Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
